3fbbe4612aa2658357153f6c15d56f2ea42ca0a6
Pietro-Savelli <pietrosavelli04@gmail.com>
2026-05-03 15:55:35 +0200

    Primo commit: inizializzazione del progetto

---
+++ src/test/java/it/uniroma3/torneidicalcio/TorneiDiCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.torneidicalcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneiDiCalcioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
